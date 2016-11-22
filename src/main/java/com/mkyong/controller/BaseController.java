d7531eadcbf87463eaf1852f0e1502ab04d9ce2d
Marek Pataky <marek.pataky@hotovo.org>
2016-11-22 12:55:36 +0100

    Env properties test.

--- src/main/java/com/mkyong/controller/BaseController.java
+++ src/main/java/com/mkyong/controller/BaseController.java
@@ -7,33 +7,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Map;
+
 @Controller
 public class BaseController {
 
-	private static int counter = 0;
-	private static final String VIEW_INDEX = "index";
-	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
+    private static int counter = 0;
+    private static final String VIEW_INDEX = "index";
+    private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
+    private final static Map<String, String> aa = System.getenv();
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String welcome(ModelMap model) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome(ModelMap model) {
+        model.addAttribute("message", "Welcome --" + System.getenv("TEST") + " --" + aa);
+        model.addAttribute("counter", ++counter);
+        logger.debug("[welcome] counter : {}", counter);
 
-		model.addAttribute("message", "Welcome");
-		model.addAttribute("counter", ++counter);
-		logger.debug("[welcome] counter : {}", counter);
+        // Spring uses InternalResourceViewResolver and return back index.jsp
+        return VIEW_INDEX;
 
-		// Spring uses InternalResourceViewResolver and return back index.jsp
-		return VIEW_INDEX;
+    }
 
-	}
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public String welcomeName(@PathVariable String name, ModelMap model) {
 
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public String welcomeName(@PathVariable String name, ModelMap model) {
+        model.addAttribute("message", "Welcome " + name);
+        model.addAttribute("counter", ++counter);
+        logger.debug("[welcomeName] counter : {}", counter);
+        return VIEW_INDEX;
 
-		model.addAttribute("message", "Welcome " + name);
-		model.addAttribute("counter", ++counter);
-		logger.debug("[welcomeName] counter : {}", counter);
-		return VIEW_INDEX;
-
-	}
+    }
 
 }
